e6aec375b08631b6674d112495670e1237a42c2d
Elies AMARELO <eamarelo@hotmail.fr>
2018-09-26 23:01:16 +0200

    add template home

---
+++ src/pages/home/components/paragraph.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import '../../../index.scss'
+
+const Paragraph = ({ data }) => (
+  <div>
+    {data.map(item => (
+      <div className="test" key={item.id}>
+        <div>
+          {`Title: ${item.title}`}
+        </div>
+        <img className="floatLeft" src={item.image} alt="img-event" height="40%" width="40%" />
+        <div>
+          {`Start: ${item.dateStart}`}
+        </div>
+        <div>
+          {`End: ${item.dateEnd}`}
+        </div>
+        <div>
+          {`City: ${item.city}`}
+        </div>
+        <div>
+          {`Address: ${item.address}`}
+        </div>
+        <div>
+          {`Description: ${item.description}`}
+        </div>
+      </div>
+    ))}
+  </div>
+)
+
+export default Paragraph
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,63 @@
-import React from 'react'
+import axios from 'axios'
+import React, { Component } from 'react'
 
-const Home = () => (
-  <div>
-    <p>Bienvenue sur notre site de recherche de sortie à Paris</p>
-  </div>
-)
+import initialState from './initial-state'
+import Paragraph from './components/paragraph'
+
+class Home extends Component {
+  constructor() {
+    super()
+
+    this.state = initialState
+  }
+
+  /**
+   * Get data
+   * @param {string} query
+   * @return {Object} dataFormatted
+   */
+  getData() {
+    const apiUrl = 'https://opendata.paris.fr/api/records/1.0/search/?dataset=evenements-a-paris&facet=placename&facet=department&facet=region&facet=city&facet=date_start&facet=date_end&facet=pricing_info'
+
+    axios.get(apiUrl)
+      .then((response) => {
+        this.setState({
+          data: this.formatEvents(response.data.records)
+        })
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  /**
+   * Format events
+   * @param {Array} events
+   * @return {Array} eventsFormatted
+   */
+  formatEvents(events) {
+    return events.map(event => ({
+      id: event.recordid,
+      address: event.fields.address,
+      city: event.fields.city,
+      dateEnd: event.fields.date_end,
+      dateStart: event.fields.date_start,
+      description: event.fields.description,
+      image: event.fields.image,
+      title: event.fields.title
+    }))
+  }
+
+  render() {
+    const { data } = this.state
+
+    return (
+      <div>
+        {this.getData()}
+        <Paragraph data={data} />
+      </div>
+    )
+  }
+}
 
 export default Home
